91bbe151b5d6db72019d96760b01d401c9da1fe8
Rafael De Leon <rafael.deleon12688@gmail.com>
2021-02-05 01:25:24 -0500

    styled components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import home1 from '../img/home1.png';
 
+// styled
+import styled from 'styled-components';
+
 const AboutSection = () => {
   return (
-    <div>
-      <div className='description'>
+    <About>
+      <Description>
         <div className='title'>
-          <div className='hide'>
+          <Hide>
             <h2>We Work to make</h2>
-          </div>
-          <div className='hide'>
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className='hide'>
+          </Hide>
+          <Hide>
             <h2>true</h2>
-          </div>
+          </Hide>
         </div>
 
         <p>
@@ -24,12 +27,42 @@ const AboutSection = () => {
           have professionals with amazing skills.
         </p>
         <button>Contact Us</button>
-        <div className='image'>
-          <img src={home1} alt='guy with a camera' />
-        </div>
-      </div>
-    </div>
+      </Description>
+      <Image>
+        <img src={home1} alt='guy with a camera' />
+      </Image>
+    </About>
   );
 };
+
+// Styled components
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 
 export default AboutSection;
