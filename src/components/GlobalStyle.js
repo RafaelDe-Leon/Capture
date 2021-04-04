15a4c49e95d3e1191e83130dac12393dcc5e8da2
Rafael De Leon <rafael.deleon12688@gmail.com>
2021-04-04 16:35:14 -0400

    styled services section

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
 
 body{
   background: #1b1b1b;
+  font-family: 'Inter', sans-serif;
 
 }
 
@@ -22,21 +23,23 @@ button {
   background: transparent;
   color: white;
   transition: all 0.5s ease;
+  font-family: 'Inter', sans-serif;
   &:hover{
     background-color: #23d997;
     color: white;
   }
   
 }
-
   h2{
-    font-weight: lighter;font-size: 4rem;
+    font-weight: lighter;
+    font-size: 4rem;
   }
   h3 {
     color: white;
   }
   h4 {
     font-weight: bold;
+
   }
   span {
     font-weight: bold;
@@ -51,6 +54,7 @@ button {
     font-size: 1.4rem;
     line-height: 150%;
   }
+
 `;
 
 export default GlobalStyle;
